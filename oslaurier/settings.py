bac974566e8bdde69acb7b0689595d489ed886de
Jeffrey Charles <jeffreycharles@gmail.com>
2009-08-29 14:56:08 -0400

    Initial commit with applications and middleware added

---
+++ oslaurier/settings.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+# Django settings for oslaurier project.
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Jeffrey Charles', 'webmaster@oslaurier.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASE_ENGINE = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'oslaurier'             # Or path to database file if using sqlite3.
+DATABASE_USER = 'oslaurier'             # Not used with sqlite3.
+DATABASE_PASSWORD = 'green'         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'America/Toronto'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = False
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = '/home/jeff/projects/osl/main-website/http/media/'
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = 'http://local.oslaurier.org/media/'
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/admin_media/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'RImQ:Yp)24e[C^B7DBnH5?*Szci>ak^;v)+XgLZSP+uj7&C?<U'
+
+# Custom settings I have added
+DEFAULT_FROM_EMAIL = 'django@oslaurier.org'
+FILE_UPLOAD_PERMISSIONS = 0666
+PREPEND_WWW = True
+SEND_BROKEN_LINK_EMAILS = True
+SERVER_EMAIL = 'django@oslaurier.org'
+SESSION_COOKIE_NAME = 'oslaurier_session'
+TEMPLATE_DEBUG = True
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+#     'django.template.loaders.eggs.load_template_source',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.transaction.TransactionMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.csrf.middleware.CsrfMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+    'django.contrib.humanize',
+    'django.contrib.redirects.middleware.RedirectFallbackMiddleware',
+    'django.middleware.http.ConditionalGetMiddleware',
+)
+
+ROOT_URLCONF = 'oslaurier.urls'
+
+TEMPLATE_DIRS = (
+    "/home/jeff/projects/osl/oslaurier/templates/",
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.flatpages',
+    'django.contrib.redirects',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+)
