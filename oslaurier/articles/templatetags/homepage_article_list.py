d92dc48fcbe2b3caccdb24f3593e262eea28db6d
Jeffrey Charles <jeffreycharles@gmail.com>
2009-10-11 01:23:38 -0400

    Add list of most recent articles to homepage
     - create template tag for listing most recent articles
     - create template for template tag
     - add template tag for listing most recent articles to homepage

---
+++ oslaurier/articles/templatetags/homepage_article_list.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from django import template
+from oslaurier.articles.models import Article
+
+register = template.Library()
+
+@register.inclusion_tag('articles/homepage_list_articles.html')
+def list_articles(number_to_return):
+    articles = (Article.objects.exclude(draft=True).exclude(hidden=True)
+        .order_by('-date_created')[:number_to_return])
+    return {'articles': articles}
