7cb77b83045a93e878eb4058b97c6c6f466cdc19
Jeffrey Charles <jeffreycharles@gmail.com>
2009-10-11 00:20:20 -0400

    Add basic homepage template and relevant urlconf setting
     - added upper and lower margins to paragraphs

--- oslaurier/urls.py
+++ oslaurier/urls.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls.defaults import *
-
 from django.contrib import admin
+from django.views.generic.simple import direct_to_template
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^$', direct_to_template, {
+        'template': 'home.html'
+    }),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
     (r'^articles/', include('oslaurier.articles.urls')),
