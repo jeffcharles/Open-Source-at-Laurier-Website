aa673b636119d62a713e0355087494ca62cf2f44
Jeffrey Charles <jeffreycharles@gmail.com>
2009-10-31 21:47:15 -0400

    Fix navigation links so they point to proper pages

--- oslaurier/urls.py
+++ oslaurier/urls.py
@@ -5,10 +5,16 @@ from django.views.generic.simple import direct_to_template
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^$', direct_to_template, {
-        'template': 'home.html'
-    }),
+    url(r'^$', direct_to_template, {'template': 'home.html'}, name="home"),
+    url(r'^about/$', direct_to_template, {'template': 'about.html'},
+        name="about"),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
     (r'^articles/', include('oslaurier.articles.urls')),
+    url(r'^development/$', direct_to_template, {'template': 'development.html'},
+        name="development"),
+    url(r'^privacy/$', direct_to_template, {'template': 'privacy.html'},
+        name="privacy"),
+    url(r'^terms/$', direct_to_template, {'template': 'terms.html'},
+        name="terms"),
 )
