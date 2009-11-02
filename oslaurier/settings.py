6808f46c58edccaaa566b764bcbd0d8fdd3c4f45
Jeffrey Charles <jeffreycharles@gmail.com>
2009-11-01 20:28:23 -0500

    Remove development settings from settings file
     - add untracked local_settings.py file for storing dev settings
    
     - remove database and secret key settings from main settings file

--- oslaurier/settings.py
+++ oslaurier/settings.py
@@ -1,20 +1,13 @@
 # -*- coding: utf-8 -*-
 # Django settings for oslaurier project.
 
-DEBUG = True
+DEBUG = False
 
 ADMINS = (
     ('Jeffrey Charles', 'webmaster@oslaurier.com'),
 )
 
 MANAGERS = ADMINS
-
-DATABASE_ENGINE = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'wluopensource'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'wluopensource'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'green'         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -35,20 +28,17 @@ USE_I18N = False
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/home/jeff/projects/osl/main-website/http/media/'
+MEDIA_ROOT = ''
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = 'http://local.oslaurier.org/media/'
+MEDIA_URL = ''
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
 ADMIN_MEDIA_PREFIX = '/admin_media/'
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'RImQ:Yp)24e[C^B7DBnH5?*Szci>ak^;v)+XgLZSP+uj7&C?<U'
 
 # Custom settings I have added
 DEFAULT_FROM_EMAIL = 'django@oslaurier.org'
@@ -56,7 +46,7 @@ FILE_UPLOAD_PERMISSIONS = 0666
 SEND_BROKEN_LINK_EMAILS = False
 SERVER_EMAIL = 'django@oslaurier.org'
 SESSION_COOKIE_NAME = 'oslaurier_session'
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -80,7 +70,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'oslaurier.urls'
 
 TEMPLATE_DIRS = (
-    "/home/jeff/projects/osl/main-website/oslaurier/templates/",
+    "",
 )
 
 INSTALLED_APPS = (
@@ -95,3 +85,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'oslaurier.articles',
 )
+
+try:
+    from local_settings import *
+except:
+    pass
