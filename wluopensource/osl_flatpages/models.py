df3130b1ae6b56ae60661a2c55a46998c03e5755
Jeffrey Charles <jeffrey.charles@wluopensource.org>
2010-07-11 14:51:27 -0400

    Add doctests for osl_flatpage get_absolute_url method

--- wluopensource/osl_flatpages/models.py
+++ wluopensource/osl_flatpages/models.py
@@ -1,10 +1,34 @@
 import markdown
 
 from django.db import models
+from django.forms import ModelForm
 
 import settings
 
 class Flatpage(models.Model):
+    """
+    Custom flatpage model.
+    
+    This is different from the default flatpage model because it uses Markdown 
+    in the content to convert up to HTML. It also allows for template tags to
+    be used in content.
+    
+    Pages are retrieved via specifying the page name in a named url in the 
+    urlconf.
+    
+    Doctests for get_absolute_url have been moved up here because they don't 
+    work when placed in the get_absolute_url docstring.
+    
+    Test that homepage works:
+    >>> home_fp = Flatpage.objects.get(page_name="Home")
+    >>> home_fp.get_absolute_url()
+    '/'
+    
+    Test that about page works:
+    >>> about_fp = Flatpage.objects.get(page_name="About")
+    >>> about_fp.get_absolute_url()
+    '/about/'    
+    """
     page_name = models.CharField(max_length=100, primary_key=True, unique=True)
     title = models.CharField(blank=True, max_length=100)
     description = models.CharField(blank=True, max_length=255)
@@ -40,4 +64,4 @@ class Flatpage(models.Model):
     def save(self, force_insert=False, force_update=False):
         self.content = markdown.markdown(self.markdown_content)
         super(Flatpage, self).save(force_insert, force_update)
-    
+
