ce9e01eef2cc92719ef321ed29fecba5030c8782
Jeffrey Charles <jeffrey.charles@wluopensource.org>
2010-07-12 10:08:57 -0400

    Add login url and template

---
+++ wluopensource/accounts/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    (r'^login/$', 'django.contrib.auth.views.login'),
+    (r'^logout/$', 'django.contrib.auth.views.logout'),
+    (r'^password_change/$', 'django.contrib.auth.views.password_change'),
+    (r'^password_change_done/$', 'django.contrib.auth.views.password_change_done'),
+    (r'^password_reset/$', 'django.contrib.auth.views.password_reset'),
+    (r'^password_reset_done/$', 'django.contrib.auth.views.password_reset_done'),
+    (r'^password_reset_confirm/$', 'django.contrib.auth.views.password_reset_confirm'),
+    (r'^password_reset_complete/$', 'django.contrib.auth.views.password_reset_complete'),
+)
