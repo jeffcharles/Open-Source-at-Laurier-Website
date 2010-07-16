2a6d22d38137f6c3e20d1c33fd9374c94ca303cf
Jeffrey Charles <jeffrey.charles@wluopensource.org>
2010-07-15 23:05:40 -0400

    Add CSS for errors on forms

--- wluopensource/accounts/forms.py
+++ wluopensource/accounts/forms.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from django.forms import ModelForm
 
 class UserInfoChangeForm(ModelForm):
+    error_css_class = 'error'
+
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email')
