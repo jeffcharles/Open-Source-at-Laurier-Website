d40336b26c1b21e5d8b137b72727933193103a65
Jeffrey Charles <jeffrey.charles@wluopensource.org>
2010-07-15 22:02:42 -0400

    Explicitly include fields on profile form rather than excluding

--- wluopensource/accounts/forms.py
+++ wluopensource/accounts/forms.py
@@ -4,7 +4,5 @@ from django.forms import ModelForm
 class UserInfoChangeForm(ModelForm):
     class Meta:
         model = User
-        exclude = ('username', 'password', 'is_staff', 'is_active', 
-            'is_superuser', 'last_login', 'date_joined', 'groups', 
-            'user_permissions')
+        fields = ('first_name', 'last_name', 'email')
     
