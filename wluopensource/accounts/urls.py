c07a68b49e4ae17e79a70975bd620e608f7a9a1a
Jeffrey Charles <jeffrey.charles@wluopensource.org>
2010-07-30 21:13:05 -0400

    Add password reset functionality

--- wluopensource/accounts/urls.py
+++ wluopensource/accounts/urls.py
@@ -1,15 +1,8 @@
 from django.conf.urls.defaults import *
+from django.contrib.auth.urls import urlpatterns
 
-urlpatterns = patterns('',
+urlpatterns += patterns('',
     (r'^create_account/$', 'accounts.views.create_account'),
-    (r'^login/$', 'django.contrib.auth.views.login'),
-    (r'^logout/$', 'django.contrib.auth.views.logout'),
-    (r'^password_change/$', 'django.contrib.auth.views.password_change'),
-    (r'^password_change_done/$', 'django.contrib.auth.views.password_change_done'),
-    (r'^password_reset/$', 'django.contrib.auth.views.password_reset'),
-    (r'^password_reset_done/$', 'django.contrib.auth.views.password_reset_done'),
-    (r'^password_reset_confirm/$', 'django.contrib.auth.views.password_reset_confirm'),
-    (r'^password_reset_complete/$', 'django.contrib.auth.views.password_reset_complete'),
     (r'^profile/$', 'accounts.views.profile'),
     (r'^profile_change/$', 'accounts.views.profile_change'),
 )
