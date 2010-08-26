2539f8adbe2b7deed2974c4245fd8087a8f05e65
Jeffrey Charles <jeffrey.charles@wluopensource.org>
2010-08-26 13:07:10 -0400

    Use signals to add authenticated user URL to comment when posted

--- wluopensource/osl_comments/models.py
+++ wluopensource/osl_comments/models.py
@@ -1,8 +1,18 @@
 from django.contrib.comments.models import Comment
+from django.contrib.comments.signals import comment_was_posted
 from django.db import models
 
 class OslComment(Comment):
     parent_comment = models.ForeignKey(Comment, blank=True, null=True, related_name='parent_comment')
     inline_to_object = models.BooleanField()
     edit_timestamp = models.DateTimeField(auto_now=True)
+
+def comment_user_url_injection_handler(sender, **kwargs):
+    if 'request' in kwargs and kwargs['request'].user.is_authenticated() and \
+        'comment' in kwargs:
+        
+        comment = kwargs['comment']
+        comment.url = comment.user.get_profile().url
+        comment.save()
+comment_was_posted.connect(comment_user_url_injection_handler)
 
