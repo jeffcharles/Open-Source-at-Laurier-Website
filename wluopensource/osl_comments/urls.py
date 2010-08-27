3aee3f32dec40dc42ea857b64eb0f31dae0db07f
Jeffrey Charles <jeffrey.charles@wluopensource.org>
2010-08-27 12:40:28 -0400

    Add reference to comment edited in urlconf

--- wluopensource/osl_comments/urls.py
+++ wluopensource/osl_comments/urls.py
@@ -3,6 +3,7 @@ from django.contrib.comments.urls import urlpatterns
 
 urlpatterns += patterns('',
     (r'^edit/$', 'osl_comments.views.edit_comment'),
+    (r'^edited/$', 'osl_comments.views.comment_edited'),
     url(r'^cr/(\d+)/(.+)/$', 'django.views.defaults.shortcut', name='comments-url-redirect'),
 )
 
