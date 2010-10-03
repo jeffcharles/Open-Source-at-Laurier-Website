48371d27608a463497c857a80b6ecd8fb2cad26d
Jeffrey Charles <jeffrey.charles@wluopensource.org>
2010-10-03 17:00:45 -0400

    Link arrows with votes and display sum of votes for comments

--- wluopensource/osl_comments/urls.py
+++ wluopensource/osl_comments/urls.py
@@ -11,27 +11,14 @@ urlpatterns += patterns('osl_comments.views',
     (r'^ip_address_ban/(?P<comment_id>\d+)/$', 'update_ip_address_ban'),
     (r'^ip_address_ban_update_done/$', 'update_ip_address_ban_done'),
     url(r'^ocr/(\d+)/(.+)/$', 'redirect_view', name='osl-comments-url-redirect'),
-    
 )
 
-comment_voting_dict = {'model': OslComment, 'template_object_name': 'comment', 
-    'allow_xmlhttprequest': True}
-
-comment_clear_voting_dict = comment_voting_dict.copy()
-comment_clear_voting_dict.update({'direction': 'clear'})
-
-comment_down_voting_dict = comment_voting_dict.copy()
-comment_down_voting_dict.update({'direction': 'down'})
-
-comment_up_voting_dict = comment_voting_dict.copy()
-comment_up_voting_dict.update({'direction': 'up'})
-
 urlpatterns += patterns('voting.views',
-    url(r'^vote/(?P<object_id>\d+)/clear/$', 'vote_on_object', 
-        comment_clear_voting_dict, name='vote-comment-clear'),
-    url(r'^vote/(?P<object_id>\d+)/down/$', 'vote_on_object', 
-        comment_down_voting_dict, name='vote-comment-down'),
-    url(r'^vote/(?P<object_id>\d+)/up/$', 'vote_on_object', 
-        comment_up_voting_dict, name='vote-comment-up'),
+    url(r'^vote/(?P<object_id>\d+)/(?P<direction>up|down|clear)/$', 
+        'vote_on_object', 
+        {'model': OslComment, 'template_object_name': 'comment', 
+        'allow_xmlhttprequest': True, 
+        'template_name': 'comments/confirm_vote.html'},
+        name='vote-comment'),
 )
 
