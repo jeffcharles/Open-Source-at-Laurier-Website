554d3de083be6ba5710776bc1bf0f91645526cae
Jeffrey Charles <jeffrey.charles@wluopensource.org>
2010-11-16 20:00:06 -0500

    Fix bug in comment list where it would not work with 0 comments

--- wluopensource/osl_comments/templatetags/osl_comments_lib.py
+++ wluopensource/osl_comments/templatetags/osl_comments_lib.py
@@ -575,7 +575,7 @@ class RenderOslCommentListNode(OslCommentListNode):
             context.push()
             
             comment_list = self.get_context_value_from_queryset(context, qs)
-            if not comment_list[0].parent:
+            if len(comment_list) > 0 and not comment_list[0].parent:
                 first_comment_parent = \
                     OslComment.objects.get(pk=comment_list[0].parent_comment_id)
             else:
