4488c2a5fe823502dd4c6dd6cb699cf180a38cf3
Jeffrey Charles <jeffrey.charles@wluopensource.org>
2010-11-24 21:31:46 -0500

    Add ajax loader when user votes on something w/ ajax call

--- media/js/voting.js
+++ media/js/voting.js
@@ -4,9 +4,12 @@ $(document).ready(function() {
         var selectedAnchor = $(this);
         var voteBox = $(selectedAnchor.parent());
         var voteBoxContainer = voteBox.parent();
+        var scoreDiv = voteBoxContainer.find("div.score-sum");
+        scoreDiv.html("<img src='/media/images/ajax-loader.gif' alt='Loading...' />");
         $.post(selectedAnchor.attr("href"), function(data) {
             voteBoxContainer.load(voteBox.attr("data-ajax-url"), function() {
-                voteBoxContainer.children("div.vote-box").children("span.score-sum").text($.parseJSON(data).score.score);
+                // need to repeat reference below b/c prev step nullifies reference to scoreDiv
+                voteBoxContainer.find("div.score-sum").html($.parseJSON(data).score.score);
             });
         });
         return false;
